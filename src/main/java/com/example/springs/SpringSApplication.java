6bfebcb49fef3d2a90a38916e5eca3685baa439a
liujinlong <liujinlong>
2019-04-23 11:41:51 +0800

    测试

---
+++ src/main/java/com/example/springs/SpringSApplication.java
@@ -0,0 +1,13 @@
+package com.example.springs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSApplication.class, args);
+    }
+
+}
